369422022e670036e37727b143c9a2638efaec45
dannasndz <dannasandez0@gmail.com>
2023-11-08 21:28:05 -0800

    Update Practica7_Punteros_SandezIslas_Danna.c

--- Practicas/PRACTICA_7/Practica7_Punteros_SandezIslas_Danna.c
+++ Practicas/PRACTICA_7/Practica7_Punteros_SandezIslas_Danna.c
@@ -43,6 +43,7 @@ int main(int argc, char *argv[])
             break;
         case 0:
             printf("Saliendo...\n");
+            system("PAUSE");
             break;
         }
         system("CLS");
